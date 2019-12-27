ee32b6ec20ab3763f41a4c332e39f295e4674dd1
dhruvshah8 <30390967+dhruvshah8@users.noreply.github.com>
2019-12-27 14:29:36 -0500

    Add files via upload

---
+++ art.java
@@ -0,0 +1,140 @@
+public class art
+{
+  
+  
+  public static void main(String[] args)
+  {
+    new art();
+  }
+  public static void cherries()
+  {
+    System.out.println("   __.--~~.,-.__");
+    System.out.println("   `~-._.-(`-.__`-.");
+    System.out.println("           \\    `~~`");
+    System.out.println("      .--./ \\");
+    System.out.println("     /#   \\  \\.--.");
+    System.out.println("     \\    /  /#   \\");
+    System.out.println("      '--'   \\    /");
+    System.out.println("              '--'");
+  } 
+  public static void potion()
+  {
+    System.out.println("  `.___,'  ");
+    System.out.println("   (___) ");
+    System.out.println("   <   >");
+    System.out.println("    ) (");
+    System.out.println("   /`-.\\");
+    System.out.println("  /     \\");
+    System.out.println(" / _    _\\");
+    System.out.println(":         :");
+    System.out.println("|         |");
+    System.out.println(" :         ;");
+    System.out.println("  \\       /");
+    System.out.println("  `.___.' ");
+  }
+  
+  public static void box()
+  {
+    System.out.println("\n         _________________________ ");
+    System.out.println("        / _____________________  /| ");
+    System.out.println("       / / ___________________/ / | ");
+    System.out.println("      / / /| |               / /  | ");
+    System.out.println("     / / / | |              / / . | ");
+    System.out.println("    / / /| | |             / / /| | ");
+    System.out.println("   / / / | | |            / / / | | ");
+    System.out.println("  / / /  | | |           / / /| | | ");
+    System.out.println(" / /_/__________________/ / / | | | ");
+    System.out.println("/________________________/ /  | | | ");
+    System.out.println("| ______________________ | |  | | | ");
+    System.out.println("| | |    | | |_________| | |__| | | ");
+    System.out.println("| | |    | |___________| | |____| | ");
+    System.out.println("| | |   / / ___________| | |_  / / ");
+    System.out.println("| | |  / / /           | | |/ / / ");
+    System.out.println("| | | / / /            | | | / / ");
+    System.out.println("| | |/ / /             | | |/ / ");
+    System.out.println("| | | / /              | | ' / ");
+    System.out.println("| | |/_/_______________| |  / ");
+    System.out.println("| |____________________| | / ");
+    System.out.println("|________________________|/");
+    
+  }
+  
+  public static void morseCode()
+  {
+    
+    System.out.println("+----------------------------------------------------+");
+    System.out.println("¦ Letter --> ¦ Morse Code  ¦ Letter --> ¦ Morse Code ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ A          ¦ ._          ¦ B          ¦ _...       ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ C          ¦ _._.        ¦ D          ¦ _..        ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ E          ¦ .           ¦ F          ¦ .._.       ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ G          ¦ _ _.        ¦ H          ¦ ....       ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ I          ¦ ..          ¦ J          ¦ . _ _ _    ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ K          ¦ _._         ¦ L          ¦ ._..       ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ M          ¦ _ _         ¦ N          ¦ _.         ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ O          ¦ _ _ _       ¦ P          ¦ ._ _ .     ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ Q          ¦ _ _ . _     ¦ R          ¦ ._.        ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ S          ¦ ...         ¦ T          ¦ _          ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ U          ¦ .._         ¦ V          ¦ ..._       ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ W          ¦ ._ _        ¦ X          ¦ _.._       ¦");
+    System.out.println("¦------------+-------------+------------+------------¦");
+    System.out.println("¦ Y          ¦ _._ _       ¦ Z          ¦ _ _..      ¦");
+    System.out.println("+----------------------------------------------------+"); 
+  }
+  
+  public static void arcade()
+  {
+    System.out.println("**************************************");
+    System.out.println("*     __  __           _             *");
+    System.out.println("*    |  \\/  |__ _ __ _(_)__          *");
+    System.out.println("*    | |\\/| / _` / _` | / _|         *");
+    System.out.println("*    |_| _|_\\__,_\\__, |_\\__|         *");
+    System.out.println("*     _  _             |              *");
+    System.out.println("*    | \\| |_  _ _|___/ |__  ___ _ _  *");
+    System.out.println("*    | .` | || | '  \\| '_ \\]/ -_) '_| *");
+    System.out.println("*    |_|\\_|\\_,_|_|_|_|_.__/\\ ___|_|   *");
+    System.out.println("*                                    *");
+    System.out.println("*         Press enter to start       *");
+    System.out.println("**************************************");
+  }
+  public static void sword()
+  {
+    System.out.println("         />_________________________________  ");
+    System.out.println("[########[]_________________________________>");
+    System.out.println("         \\> ");
+  }
+  
+  public static void ticket()
+  {
+System.out.println(" _____________________________");
+System.out.println("|                             |");
+System.out.println("|           TICKET            |");
+System.out.println("|                             |");
+System.out.println("|      Please Admit one       |");
+System.out.println("|                             |");
+System.out.println("|        FIX IT FELIX         |");
+System.out.println("`-----------------------------'");
+
+
+  }
+  public static void title()
+  {
+System.out.println("  __      __                        __     .___  __    __________        .__         .__     ");
+System.out.println("/  \\    /  \\_______   ____   ____ |  | __ |   |/  |_  \\______   \\_____  |  | ______ |  |__  ");
+System.out.println("\\   \\/\\/   /\\_  __ \\_/ __ \\_/ ___\\|  |/ / |   \\   __\\  |       _/\\__  \\ |  | \\____ \\|  |  \\ ");
+System.out.println(" \\        /  |  | \\/\\  ___/\\  \\___|    <  |   ||  |    |    |   \\ / __ \\|  |_|  |_> >   Y  \\");
+System.out.println("  \\__/\\  /   |__|    \\___  >\\___  >__|_ \\ |___||__|    |____|_  /(____  /____/   __/|___|  /");
+System.out.println("       \\/                \\/     \\/     \\/                     \\/      \\/     |__|        \\/  ");
+  }
+}
